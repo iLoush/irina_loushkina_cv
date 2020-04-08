54c4c830882fad1c1b2c6871cf333b44a931742e
iLoush <iloushkina@gmail.com>
2020-04-08 17:41:40 +0200

    index.html, style.css, main.js added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+   $('.slider__inner').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      speed: 800,
+      // autoplay: true,
+      autoplaySpeed: 2000,
+      infinite: true,
+   });
+
+});
